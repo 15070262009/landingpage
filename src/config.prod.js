9dbe37b9fbf0899369c715ea017ce203c03f1a4d
wang jun <530955809@qq.com>
2020-06-18 18:06:49 +0800

    token

--- src/config.prod.js
+++ src/config.prod.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   now: {
-    token: 'your token',
+    token: 'giC7fKzpGCP2cqxXCueDz5Dd',
     url: 'https://landingpage-topaz.vercel.app/',
     templateDir: path.join(__dirname, 'template'),
     whiteList: ['ant-design-landing-build'],
