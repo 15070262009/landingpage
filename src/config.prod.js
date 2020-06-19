77107b3fa10f243f8d0fc9771d2808cc7a61bd34
wang jun <530955809@qq.com>
2020-06-19 17:34:12 +0800

    12

--- src/config.prod.js
+++ src/config.prod.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   now: {
-    token: 'giC7fKzpGCP2cqxXCueDz5Dd',
+    token: 'AGWk8Lf7TwnuM1TMgQnGGkjO',
     url: 'https://landingpage-topaz.vercel.app/',
     templateDir: path.join(__dirname, 'template'),
     whiteList: ['ant-design-landing-build'],
--- src/template/pages/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import Home from '../components/index';
-
-export default () => <Home />;
